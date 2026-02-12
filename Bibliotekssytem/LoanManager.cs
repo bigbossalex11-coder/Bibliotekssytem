87927d1223018d544137b435eb42030f45164fdd
AlexAC <bigbossalex11@gmail.com>
2026-02-12 12:38:53 +0100

    Implement LoanManager with BorrowBooks and ReturnBooks using TDD

--- Bibliotekssytem/LoanManager.cs
+++ Bibliotekssytem/LoanManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,27 @@ namespace Bibliotekssytem
 {
     public class LoanManager
     {
+        public List<Loan> Loans { get; set; }
+
+        public LoanManager()
+        {
+            Loans = new List<Loan>();
+        }
+        public void BorrowBooks(Book book, Member member)
+        {
+            var loan = new Loan(book, member);
+            Loans.Add(loan);
+        }
+        public void ReturnBooks(Book book)
+        {
+            foreach (var loan in Loans)
+            {
+                if (loan.Book == book)
+                {
+                    loan.ReturnBook();
+                }
+            }
+        }
     }
+        
 }
---
+++ BiblioteksystemTests/LoanManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bibliotekssytem;
+using Xunit;
+
+namespace BiblioteksystemTests
+{
+    public class LoanManagerTest
+    {
+        [Fact]
+        public void BorrowBooks_Ads_Book_And_Member_To_List()
+        {
+            var loanManager = new LoanManager();
+            var book = new Book("123-321", "Hobbit", "Tolkien", 1937);
+            var member = new Member("Charlie", "0001", true);
+
+            loanManager.BorrowBooks(book, member);
+
+            Assert.Single(loanManager.Loans);
+        }
+        [Fact]
+        public void ReturnBook_Marks_Book_As_Returned_as_True()
+        {
+            var loanManager = new LoanManager();
+            var book = new Book("123-321", "Hobbit", "Tolkien", 1937);
+            var member = new Member("Charlie", "0001", true);
+
+            loanManager.BorrowBooks(book,member);
+            loanManager.ReturnBooks(book);
+
+            Assert.True(loanManager.Loans[0].IsReturned);
+        }
+
+    }
+}
