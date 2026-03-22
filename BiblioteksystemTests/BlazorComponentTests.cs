33db5eafe80b0e80d7cd1b2533a341adf55407b6
AlexAC <bigbossalex11@gmail.com>
2026-03-22 13:16:53 +0100

    Add bUnit component tests for Books and Members pages

---
+++ BiblioteksystemTests/BlazorComponentTests.cs
@@ -0,0 +1,66 @@
+﻿using Bunit;
+using LibrarySystem.Core;
+using LibrarySystem.Data.Interfaces;
+using LibrarySystem.Web.Components.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+
+namespace BiblioteksystemTests
+{
+    public class BlazorComponentTests : BunitContext
+    {
+        [Fact]
+        public void Books_ShouldRenderHeading()
+        {
+            // Arrange 
+            var mockRepo = new Mock<IBookRepository>();
+            mockRepo.Setup(r => r.GetAllAsync())
+                .ReturnsAsync(new List<Book>());
+
+            Services.AddSingleton(mockRepo.Object);
+
+            // Act 
+            var cut = Render<Books>();
+
+            // Assert 
+            Assert.Contains("Böcker", cut.Find("h1").TextContent);
+        }
+
+        [Fact]
+        public void Books_ShouldDisplayBookInTable()
+        {
+            //Arrange
+            var book = new Book("123-321", "Hobbit", "Tolkien", 1937);
+            var books = new List<Book> { book };
+            var mockRepo = new Mock<IBookRepository>();
+            mockRepo.Setup(r => r.GetAllAsync())
+                .ReturnsAsync(books);
+
+            Services.AddSingleton(mockRepo.Object);
+
+            //Act
+            var cut = Render<Books>();
+
+            //Assert
+            Assert.Contains("Hobbit", cut.Find("tbody").TextContent);
+        }
+        [Fact]
+        public void Member_ShouldRenderHeading()
+        {
+            //Arrange
+            var mockRepo = new Mock<IMemberService>();
+            mockRepo.Setup(r => r.GetActiveMemberAsync())
+                .ReturnsAsync(new List<Member>());
+
+            Services.AddSingleton(mockRepo.Object);
+            
+            //Act
+            var cut = Render<Members>();
+
+            //Assert
+            Assert.Contains("Medlem", cut.Find("h1").TextContent);
+        }
+    }
+}
